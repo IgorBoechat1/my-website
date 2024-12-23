e001eeecc21266939190c2f31715e12d9380a41e
IgorBoechat1 <igorcboechatl@gmail.com>
2024-12-23 22:29:48 +0000

    Deploy

--- my-website/next.config.ts
+++
@@ -1,9 +0,0 @@
-// next.config.ts
-import type { NextConfig } from 'next';
-
-const nextConfig: NextConfig = {
-  reactStrictMode: true,
-  // Add other settings here
-};
-
-export default nextConfig;
