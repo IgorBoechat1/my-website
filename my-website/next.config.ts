38cdc0fc5e963979670227e76b9248d1277e78de
IgorBoechat1 <igorcboechatl@gmail.com>
2024-12-23 21:06:54 +0000

    Deploy

--- my-website/next.config.ts
+++ my-website/next.config.ts
@@ -1,7 +1,13 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+// next.config.js
+module.exports = {
+  reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/old-page',
+        destination: '/new-page',
+        permanent: true,
+      },
+    ];
+  },
 };
-
-export default nextConfig;
