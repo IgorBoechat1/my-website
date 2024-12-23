538ad99510eda2793966028f4839e6ec808743f4
IgorBoechat1 <igorcboechatl@gmail.com>
2024-12-23 23:12:47 +0000

    Deploy

--- my-website/src/app/layout.tsx
+++ my-website/src/app/layout.tsx
@@ -12,7 +12,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <head>
         <title>Your Website</title>
         <meta charSet="UTF-8" />
-        <link rel="stylesheet" href="/globals.css" />
+        <link rel="stylesheet" href="./globals.css" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </head>
       <body>
