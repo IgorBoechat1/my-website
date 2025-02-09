c64cbd171809a4555573dc1bc77ba8b43c2d85a5
IgorBoechat1 <igorcboechatl@gmail.com>
2025-02-09 14:55:33 +0000

    Favicon change

--- my-website/src/app/layout.tsx
+++ my-website/src/app/layout.tsx
@@ -13,6 +13,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <title>Igor Boechat</title>
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="icon" href="/favigor.svg" />
       </head>
       <body>
         <Providers>
