a1731f32b25ae6dd598af000b7b86fa6583ecf6c
IgorBoechat1 <igorcboechatl@gmail.com>
2025-02-10 20:47:58 +0000

    add thumbnail

--- my-website/src/app/layout.tsx
+++ my-website/src/app/layout.tsx
@@ -4,7 +4,7 @@
 import React from "react";
 import { Navbar } from "@/components/Navbar";
 import Providers from "@/components/Providers";
-import "./globals.css"; 
+import "./globals.css";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
@@ -14,6 +14,12 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="icon" href="/favigor.svg" />
+        {/* Primary Meta Tags */}
+        <meta name="title" content="Igor Boechat" />
+        <meta name="description" content="Welcome to Boechat's portfolio. Explore my projects and portfolio." />
+        <meta name="image" content="/thumbnail.png" />
+
+
       </head>
       <body>
         <Providers>
